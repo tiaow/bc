e7ee3826dbb6753049e703665f34ef4635ab0bcb
tiaow <3696812003@qq.com>
2025-01-14 13:15:39 +0800

    Add files via upload

--- 脚本.lua
+++ 脚本.lua
@@ -61,6 +61,53 @@ local credits = creds:section("通用",true)
             game.Lighting.Ambient = Color3.new(0, 0, 0)
         end
     end)
+    credits:Button(
+    "甩飞所有人",
+    function()
+    local cam = workspace.CurrentCamera
+local RS = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local Flashlight = RS.Flashlight
+local Clone = Flashlight:Clone()
+Clone.Parent = script.Parent
+
+local Brightness = 5
+
+local Keybind = Enum.KeyCode.F
+
+local UIS = game:GetService("UserInputService")
+
+local Toggle = false
+
+local Mouse = game.Players.LocalPlayer:GetMouse()
+
+local TS = game:GetService("TweenService")
+local TI = TweenInfo.new(.1, Enum.EasingStyle.Sine)
+
+
+UIS.InputBegan:Connect(function(Input, p)
+	if p then return end
+	if Input.KeyCode == Keybind then
+		Toggle = not Toggle
+	end
+end)
+
+RunService.RenderStepped:Connect(function()
+	if Clone then
+		
+		Clone.Position = cam.CFrame.Position
+		TS:Create(Clone, TI, {CFrame = CFrame.lookAt(Clone.Position, Mouse.Hit.Position)}):Play()
+		
+		if Toggle then
+			TS:Create(Clone.SpotLight, TI, {Brightness = Brightness}):Play()
+			
+		else
+			TS:Create(Clone.SpotLight, TI, {Brightness = 0}):Play()
+		end
+
+	end
+end)
+    
 local creds = window:Tab("传送",'16060333448')             
 local credits = creds:section("传送功能",true)
     if getgenv().ED_AntiKick then
